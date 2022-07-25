a43603049224666e314ef8b4db10f6a3f77c1507
dnyanesh19 <109947343+dnyanesh19@users.noreply.github.com>
2022-07-25 13:59:27 +0530

    write a program to find evenorodd

---
+++ Evenorodd.java
@@ -0,0 +1,29 @@
+/*Java Program to Check Whether Number is Even or Odd
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author India
+ */
+public class Evenorodd {
+    
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    
+}
+}
